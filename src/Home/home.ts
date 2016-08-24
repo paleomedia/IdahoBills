c1952c8a0deac48d7e6afa86d9fb49146e390f0a
Jason Woods <jdwoodsy1@gmail.com>
2016-08-24 09:17:19 -0600

    fixed cors issue

--- src/Home/home.ts
+++ src/Home/home.ts
@@ -14,6 +14,10 @@ postComment(bill, comment){
 }
 
 getBills(){
+this.http.configure(x => {
+    x.withHeader('Access-Control-Allow-Origin', '*');
+});
+
 this.http.get('http://openstates.org/api/v1/bills/?state=id&apikey=bcc2a830883c4f459dbffe94b2a3e90f')
   .then(data => {
     this.bills = JSON.parse(data.response);
